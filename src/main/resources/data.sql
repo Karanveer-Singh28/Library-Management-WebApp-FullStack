b2cae27b71bb5cf92196bfcc027a21e9e3686e06
Karanveer Singh <ks393363@gmail.com>
2024-06-14 08:10:09 -0700

    Initial Commit

---
+++ src/main/resources/data.sql
@@ -0,0 +1,5 @@
+INSERT INTO USERS (id, password, username, email, phone_number, address, account_balance)
+VALUES (1001, 'dummy', 'Karan', 'ks393363@gmail.com', 1234456789, '2334 jdfhd,djflj', 0.0);
+
+INSERT INTO BOOKS (id, Title, Author, Quantity, status)
+VALUES (1, 'The Great Gatsby', 'F. Scott Fitzgerald', 10, 'NORMAL');
